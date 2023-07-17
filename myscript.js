c1425cefc64841120276f136e642e9bf353bb04f
JATINAS <132319239+JATINAS@users.noreply.github.com>
2023-07-16 22:18:14 -0700

    subir repositorio
    diseno principal ya esta terminado, faltan las funciones de javascript

---
+++ myscript.js
@@ -0,0 +1,18 @@
+ciclo = 0;
+function registro() {
+    const abc = quehacer
+    const tarea = {titulo: quehacer, descripcion: detalles};
+    document.getElementById("demo").innerHTML = tarea.titulo.value;
+    document.getElementById("demo1").innerHTML = tarea.descripcion.value;
+    /*var dato = document.getElementById("homework");
+    document.getElementById("demo").innerHTML = dato.options[dato.selectedIndex].text;*/
+    ciclo += 1;
+    
+}
+
+if (ciclo > 0) {
+    function editar() {
+        delete innerHTML.quehacer;
+        delete Text.quehacer;
+    }
+}
